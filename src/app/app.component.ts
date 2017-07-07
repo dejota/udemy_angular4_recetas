34810762b343d9852c051d0db462faf7246d2679
Natali-PC\juancarlos <juanquinterodj@gmail.com>
2017-07-07 17:47:47 -0300

    two-data binding entre header.component y app.component, emitter, output() y ngif para mostrar y ocultar. https://www.udemy.com/the-complete-guide-to-angular-2/learn/v4/t/lecture/6656136?start=0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  // crea propiedad para guardar el valor que trae cuando clickea en header html, y lo inicializa con recipe para mostrarlo en la app.
+  loadedFeature = 'recipe';
+
+  // aca recibe en el parametro el valor de cuando clickeo en header html
+  onNavigate(feature: string) {
+    // asigna el valor que trae en loadedFeature.
+    this.loadedFeature = feature;
+  }
 }
