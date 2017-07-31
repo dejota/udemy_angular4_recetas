5888b2132a0cb2bcc43ade1921af207fc03ef4d8
Natali-PC\juancarlos <juanquinterodj@gmail.com>
2017-07-31 12:41:59 -0300

    cuando se hace clic en la receta a la derecha aparece el listado de Ingredientes. https://www.udemy.com/the-complete-guide-to-angular-2/learn/v4/t/lecture/6656244?start=0

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,17 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
+    // importa arriba el modelo de ingredient para usarlo en este modelo.
+    public ingredients: Ingredient[];
 
-    constructor(name: string, desc: string, imagePath: string) {
+    //cuando se crea una nueva instancia de este modelo pasa al constructor los valores y estos se asignan a las variables creadas arriba.
+    constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
+        this.ingredients = ingredients;
     }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable()
 export class RecipeService {
@@ -9,8 +10,27 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is a description', 'http://del.h-cdn.co/assets/15/28/980x490/landscape-1436393143-gettyimages-184130285.jpg'),
-    new Recipe('Another test recipe', 'This is a description', 'http://www.landeffects.ca/Images/BLOG/Brick-Pizza-Oven.jpg')
+    new Recipe(
+      'Hamburguesa',
+      'de carne, cebolla, etc',
+      'http://del.h-cdn.co/assets/15/28/980x490/landscape-1436393143-gettyimages-184130285.jpg',
+    // le pasa un array de ingredientes  
+    [
+      new Ingredient('Carne', 1),
+      new Ingredient('Cebolla', 1),
+      new Ingredient('Tomate', 1),
+      new Ingredient('Lechuga', 1)
+    ]),
+    new Recipe(
+      'Pizza',
+      'de muzzarella a la piedra',
+      'http://www.landeffects.ca/Images/BLOG/Brick-Pizza-Oven.jpg',
+    [
+      new Ingredient('Harina', 1),
+      new Ingredient('Tomate', 6),
+      new Ingredient('Muzzarella', 1),
+      new Ingredient('Aceite de Oliva', 1)
+    ])
   ];
 
   // crea este metodo para que devuelva el array recipes, y no puedan acceder desde afuera.
