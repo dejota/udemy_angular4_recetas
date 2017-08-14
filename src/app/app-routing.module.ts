735968d73792bb5c58bf909d005e84489ae0230e
OMEGA\Juan.Quintero <juanquinterodj@gmail.com>
2017-08-14 17:23:34 -0300

    Creado app-routing donde van las rutas, importado en la app principal y para que se muestre el componente se usar router-outlet https://www.udemy.com/the-complete-guide-to-angular-2/learn/v4/t/lecture/6656376?start=0
    Tambien le quite de index.html en base href el / para que cuando compile no tire error la app.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+// crea una constante que es del tipo Routes
+// importa Routes para poder crear las rutas
+// a Routes le da valor de objetos de javascript, que van a ser las rutas que se pueden navegar.
+const appRoutes: Routes = [
+  /* cuando entra a home lo redirige a /recipes, para solucionar que tire error,
+  porque angular no entiende que es solo para home, hay que agregar pathMatch */
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'shopping-list', component: ShoppingListComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    // para configurar el RouterModule se le pasa appRoutes dentro del metodo forRoot, y asi angular sabe como entender esas rutas.
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    // para que estas rutas funcionen, hay que exportarlas a la app principal osea app.module.ts
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,9 @@ import { DropdownDirective } from './shared/dropdown.directive';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    // importa app-routing.module.ts donde se configuran las rutas.
+    AppRoutingModule
   ],
   // provee el servicio, porque tiene que pasar data desde shopping-list a recipes.
   providers: [ShoppingListService],
