1c5bea34edeafaff197895d4cf8a74c3fbc2b4e3
OMEGA\Juan.Quintero <juanquinterodj@gmail.com>
2017-08-14 17:35:24 -0300

    una vez creadas las rutas, se arreglo los links que hay en header y se usa routerlink https://www.udemy.com/the-complete-guide-to-angular-2/learn/v4/t/lecture/6656380?start=0

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,15 +6,6 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  // Output recibe info de otro componente (en este caso lo usa app.component.html), cambiando el comportamiento de encapsulamiento de cada componente, importarlo arriba.
-  // crea una propiedad featureSelected y emite el evento al usar new y EventEmitter y le dice que tipo de dato emite.
-  @Output() featureSelected = new EventEmitter<string>();
-
-  /* Metodo que llama al hacer clic en header html */
-  onSelect(feature: string) {
-    // cuando hace click pasa el parametro y lo guarda en el featureSelected
-    this.featureSelected.emit(feature);
-  }
 
   constructor() { }
 
